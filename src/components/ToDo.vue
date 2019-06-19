a6d880d867a0a24cb73b96c1124dc42d6aa656bc
Yannick <postyannick@hotmail.com>
2019-06-19 13:38:57 +0200

    Form and Validation

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <transition name='alert-in'>
+        <p class="alert" v-if="errors.has('todo')">{{ errors.first('todo') }}</p>
+      </transition>
+    
+      <form @submit.prevent="addToDo">
+        <input type="text" placeholder="Create a new thing to do.." v-model="todo" v-validate="'min:2'" name="todo"/>
+      <!-- <input type="checkbox" id="checkbox" v-model="checked">Important! -->
+     
+      </form>
       <p>These are the things you'll have to do</p>
       <ul>
         <li v-for="(data, index) in todos" :key='index'>{{ data.todo }}</li>
@@ -8,7 +17,6 @@
 
       <p v-if="todos.length == 0">You're free!</p>
       <p v-else>Get your shit together and finish some stuff!</p>
-
 
     </div>
   </div>
@@ -18,11 +26,28 @@
 export default {
   name: 'ToDo',
   data() {
+    
     return {
+      // checked: false,
+      todo: '',
       todos: [
         { "todo": "Specialize me"},
         { "todo": "Sector verdieping"}
       ]
+    }
+  },
+  methods: {
+    addToDo() {
+      this.$validator.validateAll().then((result) => {
+        if(result){
+          this.todos.push({todo: this.todo}),
+          this.todo = '';
+        } else {
+          console.log('notValid');
+        }
+      });
+ 
+      // console.log('Checkbox is '+this.checked);
     }
   }
 }
