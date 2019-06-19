2ad4ee55cfd4f3005861e9d95b6da190dc9fdb83
Yannick <postyannick@hotmail.com>
2019-06-19 12:41:26 +0200

    Vue Getting started

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDo msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./components/css/normalize.css" scoped></style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in todos" :key='index'>{{ index }}. {{ data.todo }}</li>
+      </ul> 
+
+      <p v-if="todos.length == 0">You're free!</p>
+      <p v-else>Get your shit together and finish some stuff!</p>
+
+      <div v-bind:class="{ alert: !showAlert }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data() {
+    return {
+      todos: [
+        { "todo": "Specialize me"},
+        { "todo": "Sector verdieping"}
+      ],
+      showAlert: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./css/ToDo.css" scoped></style>
